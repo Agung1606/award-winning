fed9ae37b55ee801e55ccd7296426344ecc2b0bf
Agung Saputra <agngsptra928@gmail.com>
2024-12-27 12:18:31 +0000

    create Navbar.tsx done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 import About from './components/About'
 import Features from './components/Features'
-import Hero from './components/Hero'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <NavBar />
       <Hero />
       <About />
       <Features />
